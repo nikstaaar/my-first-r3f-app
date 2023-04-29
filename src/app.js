3c404fa432b2b04ab6634ea2ce062f82a19e27b9
Your Name <niktjawalla922@gmail.com>
2023-04-29 12:14:43 +0200

    add box

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import React from "react"
 export default function App () 
 {
  return (
-<mesh>
-     <torusKnotGeometry />
-     <meshNormalMaterial />
+<mesh rotation-y={Math.PI * 0.25} position-x={ 2 } scale={ 1.5 }>
+     <boxGeometry scale={1.5}/>
+     <meshBasicMaterial color={"mediumpurple"} wireframe={ true }/>
 </mesh>
   )
 }
