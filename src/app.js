0bcd840dc3f3820ab3cbd247b1d3b0766c4ae6e1
Your Name <niktjawalla922@gmail.com>
2023-04-29 16:36:18 +0200

    add light

--- src/app.js
+++ src/app.js
@@ -19,22 +19,25 @@ export default function App ()
 
  return (
 <>
-<orbitControls args={[ camera, gl.domElement]}/>
+<orbitControls args={ [ camera, gl.domElement] }/>
+
+<directionalLight position={ [1, 2, 3] } intensity={ 1.5 }/>
+<ambientLight intensity={0.5}/>
 
 <group ref={ groupRef }>
 <mesh position-x={ -2}>
     <sphereGeometry/>
-    <meshBasicMaterial color="orange"/>
+    <meshStandardMaterial color="orange"/>
 </mesh>
 
 <mesh ref={ cubeRef } rotation-y={Math.PI * 0.25} position-x={ 2 } scale={ 1.5 }>
      <boxGeometry scale={1.5}/>
-     <meshBasicMaterial color={"mediumpurple"} wireframe={ true }/>
+     <meshStandardMaterial color={"mediumpurple"} wireframe={ true }/>
 </mesh>
 
 <mesh position-y={-1} rotation-x={ - Math.PI * 0.5} scale ={10}>
     <planeGeometry/>
-    <meshBasicMaterial color="greenyellow"/>
+    <meshStandardMaterial color="greenyellow"/>
 </mesh>
 </group>
 </>
