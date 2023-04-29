1a6b7a73a33cc47d4ff7d5db1e90a8bcb2add71b
Your Name <niktjawalla922@gmail.com>
2023-04-29 11:19:59 +0200

    setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line
+import React from 'react'
+
+import './style.css'
+import ReactDOM from 'react-dom'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <>
+        Soon to be a badass R3F application
+    </>
+)
