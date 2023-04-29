aabbf687cecb43bdcc8482c6d01179d869d07521
Your Name <niktjawalla922@gmail.com>
2023-04-29 17:29:03 +0200

    add canvas settings

--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@ import React from 'react'
 import './style.css'
 import ReactDOM from 'react-dom'
 import { Canvas } from '@react-three/fiber'
+import * as THREE from 'three'
 
 import App from './app'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas>
+    <Canvas 
+        gl={ {
+            antialias: true,
+            toneMapping: THREE.ACESFilmicToneMapping,
+            outputEncoding: THREE.sRGBEncoding
+        }}
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ 3, 2, 6]
+        }}
+    >
         <App/>
     </Canvas>
 )
